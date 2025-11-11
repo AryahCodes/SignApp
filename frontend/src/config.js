7892d971ce2b2b176c87402c2c3ece5f78b79376
Aryah Babu <aryahbabu@gmail.com>
2025-11-11 10:32:48 -0500

    fixed

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,5 @@
-cat > config.js << 'EOF'
 const BACKEND_URL = process.env.NODE_ENV === 'production' 
   ? 'https://signapp-backend-5opq.onrender.com'
   : 'http://localhost:5001';
 
 export default BACKEND_URL;
-EOF
