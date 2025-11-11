c5fecc8520dfb20a89db89a767622f1667afdf66
Aryah Babu <aryahbabu@gmail.com>
2025-11-11 10:02:37 -0500

    render

--- frontend/src/config.js
+++
@@ -1,5 +0,0 @@
-const BACKEND_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://signapp-backend.onrender.com'  // Your Render URL
-  : 'http://localhost:5001';
-
-export default BACKEND_URL;
